afbf8e6e26a7fcd1a9553b1fff462ee7aa2c9bc2
Jedrick Cosmiano <jcosmiano35@adssu.edu.ph>
2026-06-19 19:43:48 +0800

    Add welcome message and page load greeting

---
+++ script.js
@@ -0,0 +1,7 @@
+// Welcome Message
+console.log("Welcome to Jedrick's Portfolio!");
+
+// Display greeting when page loads
+window.onload = function () {
+    alert("Welcome to My Portfolio Website!");
+};
